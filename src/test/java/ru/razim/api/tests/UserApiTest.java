29d1fe80001e32f58162abe709f1d1083550bc49
Разим Андарьянов <razim6802@gmail.com>
2026-08-14 16:04:27 +0500

    Improve DLC deletion test assertions

--- src/test/java/ru/razim/api/tests/UserApiTest.java
+++ src/test/java/ru/razim/api/tests/UserApiTest.java
@@ -446,8 +446,8 @@ public class UserApiTest extends BaseTest {
                         response.getRegisterData().getGameId()
                 );
 
-        assertThat(gameResponse.getDlcs().length, equalTo(0));
         assertThat(gameResponse, notNullValue());
+        assertThat(gameResponse.getDlcs().length, equalTo(0));
     }
 
     @Test
